5f961ddd0f5c42a18f1d2aabfe8f7bb1d75efa11
Vicky Pagare <vickypagare31@gmail.com>
2024-08-30 20:09:11 +0530

    Added Task class

---
+++ src/main/java/com/ToDoList/TodoListApp/entity/Task.java
@@ -0,0 +1,51 @@
+package com.ToDoList.TodoListApp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+
+    @Enumerated(EnumType.STRING)//Used to specify how an enum type should be stored in database
+    private TaskStatus status;
+
+    private LocalDate dueDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    //Many tasks can be associated with the one user.
+    @JoinColumn(name = "user_id") //Used to define foreign key column in the database.
+    private User user;
+
+    @PrePersist //Triggers a method before the entity is inserted into the database
+    protected void onCreate()
+    {
+        createdAt=LocalDateTime.now(); //Sets the creation time
+    }
+
+    @PreUpdate //Triggers a method before the entity is updated in the database
+    protected void onUpdate()
+    {
+        updatedAt=LocalDateTime.now(); //Sets the last updated time
+    }
+
+}
