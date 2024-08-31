6f7a38ceb61ff8b78602c3b3161ededd5d9e52ce
Vicky Pagare <vickypagare31@gmail.com>
2024-08-31 17:41:53 +0530

    datatype updated

--- src/main/java/com/ToDoList/TodoListApp/entity/User.java
+++ src/main/java/com/ToDoList/TodoListApp/entity/User.java
@@ -17,7 +17,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private String username;
     private String password;
---
+++ src/main/java/com/ToDoList/TodoListApp/repository/TaskRepository.java
@@ -0,0 +1,2 @@
+package com.ToDoList.TodoListApp.repository;public interface TaskRepository {
+}
