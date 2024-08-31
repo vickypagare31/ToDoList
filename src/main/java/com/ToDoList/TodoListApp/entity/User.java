c1c19905013ef7b1dd142df14f2cdd78968a4ee8
Vicky Pagare <vickypagare31@gmail.com>
2024-08-31 16:22:33 +0530

    User class added

---
+++ src/main/java/com/ToDoList/TodoListApp/entity/User.java
@@ -0,0 +1,29 @@
+package com.ToDoList.TodoListApp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String password;
+    private String email;
+
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks;
+
+}
