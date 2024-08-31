b3cd33611aa188dec6a43e0ee2775efa4dc880ea
Vicky Pagare <vickypagare31@gmail.com>
2024-08-31 17:52:30 +0530

    Updated repositories

--- src/main/java/com/ToDoList/TodoListApp/repository/TaskRepository.java
+++ src/main/java/com/ToDoList/TodoListApp/repository/TaskRepository.java
@@ -1,2 +1,12 @@
-package com.ToDoList.TodoListApp.repository;public interface TaskRepository {
+package com.ToDoList.TodoListApp.repository;
+
+import com.ToDoList.TodoListApp.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task>findByUserId(Long userId);
+
 }
